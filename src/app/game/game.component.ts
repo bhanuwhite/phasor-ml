f64a2428e502a1afa32d8444556feb0de4cff538
shivam <shivam.ahex@gmail.com>
2022-04-18 16:13:17 +0530

    dragend issue resolved

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -10,15 +10,15 @@ class NewScene extends Phaser.Scene {
   hueso: any;
   huesoCopy: any;
   flecha: any;
-  result:any;
+  result: any;
   debug: any;
-  
+
   constructor() {
     super({ key: 'new' });
   }
 
-   preload() {
-   
+  preload() {
+
     this.load.image('p1', 'assets/images/1stPanel/1.png');
     this.load.image('p2', 'assets/images/1stPanel/2.png');
     this.load.image('p3', 'assets/images/1stPanel/3.png');
@@ -31,134 +31,179 @@ class NewScene extends Phaser.Scene {
     this.load.image('p8', 'assets/images/2ndPanel/3.png');
     this.load.image('p9', 'assets/images/2ndPanel/4.png');
     this.load.image('p10', 'assets/images/2ndPanel/5.png');
-   
-
-    
-}
 
 
- create ()
-{
-    let image1= this.add.sprite(100, 200, 'p1').setInteractive();
-    let image2= this.add.sprite(100, 300, 'p2').setInteractive();
-    let image3= this.add.sprite(100, 400, 'p3').setInteractive();
-    let image4= this.add.sprite(100, 500, 'p4').setInteractive();
-    let image5= this.add.sprite(100, 600, 'p5').setInteractive();
+
+  }
 
 
-    let image6= this.add.sprite(700, 200, 'p6').setInteractive();
-    let image7= this.add.sprite(700, 300, 'p7').setInteractive();
-    let image8= this.add.sprite(700, 400, 'p8').setInteractive();
-    let image9= this.add.sprite(700, 500, 'p9').setInteractive();
-    let image10= this.add.sprite(700, 600, 'p10').setInteractive();
+  create() {
+    let image1 = this.add.sprite(100, 200, 'p1').setInteractive();
+    let image2 = this.add.sprite(100, 300, 'p2').setInteractive();
+    let image3 = this.add.sprite(100, 400, 'p3').setInteractive();
+    let image4 = this.add.sprite(100, 500, 'p4').setInteractive();
+    let image5 = this.add.sprite(100, 600, 'p5').setInteractive();
+
+
+    let image6 = this.add.sprite(700, 200, 'p6').setInteractive();
+    let image7 = this.add.sprite(700, 300, 'p7').setInteractive();
+    let image8 = this.add.sprite(700, 400, 'p8').setInteractive();
+    let image9 = this.add.sprite(700, 500, 'p9').setInteractive();
+    let image10 = this.add.sprite(700, 600, 'p10').setInteractive();
 
     this.input.setDraggable(image6);
     this.input.setDraggable(image7);
     this.input.setDraggable(image8);
     this.input.setDraggable(image9);
     this.input.setDraggable(image10);
-// this.input.se
+    // this.input.se
 
-  
-    
+
+
     this.input.dragDistanceThreshold = 16;
 
     this.input.on('dragstart', function (pointer, gameObject) {
-// console.log(pointer, gameObject)
-        gameObject.setTint(0xff0000);
+      // console.log(pointer, gameObject)
+      gameObject.setTint(0xff0000);
 
     });
 
     this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
-// console.log(dragX, dragY)
-        gameObject.x = dragX;
-        gameObject.y = dragY;
+      // console.log(dragX, dragY)
+      gameObject.x = dragX;
+      gameObject.y = dragY;
 
     });
 
     this.input.on('dragend', function (pointer, gameObject) {
-console.log(pointer, gameObject);
-let mapping=[{key:'p1',val:200, match:'p8'},{key:'p2',val:200, match:'p7'},{key:'p3',val:200,match:'p6'},{key:'p4',val:200, match:'p10'},{key:'p5',val:200, match:'p9'}]
-let obj = mapping.find(o => o.match === gameObject.texture.key);
-console.log(obj)
-if(gameObject.x>250)
-alert('wrong move')
-else
-gameObject.disableInteractive()
-// this.input.setDraggable(image6, false);
-        gameObject.clearTint();
+      console.log(pointer, gameObject);
+      let mapping = [{ key: 'p1', val: 200, match: 'p8' }, { key: 'p2', val: 200, match: 'p7' }, { key: 'p3', val: 200, match: 'p6' }, { key: 'p4', val: 200, match: 'p10' }, { key: 'p5', val: 200, match: 'p9' }]
+      let obj = mapping.find(o => o.match === gameObject.texture.key);
+      console.log(obj)
+
+      if (obj.match == 'p9') {
+        if (gameObject.y < 605 && gameObject.y > 595) {
+          gameObject.disableInteractive()
+        }
+        else {
+          alert('wrong move')
+        }
+      }
+
+      if (obj.match == 'p8') {
+        if (gameObject.y < 205 && gameObject.y > 195) {
+          gameObject.disableInteractive()
+        }
+        else {
+          alert('wrong move')
+        }
+      }
+
+      if (obj.match == 'p7') {
+        if (gameObject.y < 305 && gameObject.y > 295) {
+          gameObject.disableInteractive()
+        }
+        else {
+          alert('wrong move')
+        }
+      }
+
+      if (obj.match == 'p6') {
+        if (gameObject.y < 405 && gameObject.y > 395) {
+          gameObject.disableInteractive()
+        }
+        else {
+          alert('wrong move')
+        }
+      }
+
+      if (obj.match == 'p10') {
+        if (gameObject.y < 505 && gameObject.y > 495) {
+          gameObject.disableInteractive()
+        }
+        else {
+          alert('wrong move')
+        }
+      }
+
+      // if(gameObject.x>250)
+      // alert('wrong move')
+      // else
+      // gameObject.disableInteractive()
+      // // this.input.setDraggable(image6, false);
+      //         gameObject.clearTint();
 
     });
-}
+  }
 
 
 
-//  create() {
- 
-//     this.add.sprite(0, 0, 'grid');
+  //  create() {
 
-//     let group = this.add.group();
-// // @ts-ignore
-//     group.inputEnableChildren = true;
+  //     this.add.sprite(0, 0, 'grid');
 
-//     let atari = group.create(32, 100, 'atari');
+  //     let group = this.add.group();
+  // // @ts-ignore
+  //     group.inputEnableChildren = true;
 
-
-
-  
-//     atari.inputEnabled = true;
-//   //  atari.input.enableDrag();
-//     atari.events.onDragStart.add(this.onDragStart, this);
-//     atari.events.onDragStop.add(this.onDragStop, this);
-
-//     let sonic = group.create(300, 200, 'sonic');
-
-//     sonic.inputEnabled = true;
-//     //sonic.input.enableDrag();
-//     sonic.events.onDragStart.add(this.onDragStart, this);
-//     sonic.events.onDragStop.add(this.onDragStop, this);
-// // @ts-ignore
-//     group.onChildInputDown.add(this.onDown, this);
-
-// }
-
-//  onDown(sprite, pointer) {
-
-//     this.result = "Down " + sprite.key;
-
- 
-//     console.log('down', sprite.key);
-
-// }
-
-//  onDragStart(sprite, pointer) {
-
-//     this.result = "Dragging " + sprite.key;
-
-// }
-
-//  onDragStop(sprite, pointer) {
-
-//     this.result = sprite.key + " dropped at x:" + pointer.x + " y: " + pointer.y;
-
-//     if (pointer.y > 400)
-//     {
-//         console.log('input disabled on', sprite.key);
-//         sprite.input.enabled = false;
-
-//         sprite.sendToBack();
-//     }
-
-// }
-
-//  render() {
-
-//     this.debug.text(this.result, 10, 20);
-
-// }
+  //     let atari = group.create(32, 100, 'atari');
 
 
- 
+
+
+  //     atari.inputEnabled = true;
+  //   //  atari.input.enableDrag();
+  //     atari.events.onDragStart.add(this.onDragStart, this);
+  //     atari.events.onDragStop.add(this.onDragStop, this);
+
+  //     let sonic = group.create(300, 200, 'sonic');
+
+  //     sonic.inputEnabled = true;
+  //     //sonic.input.enableDrag();
+  //     sonic.events.onDragStart.add(this.onDragStart, this);
+  //     sonic.events.onDragStop.add(this.onDragStop, this);
+  // // @ts-ignore
+  //     group.onChildInputDown.add(this.onDown, this);
+
+  // }
+
+  //  onDown(sprite, pointer) {
+
+  //     this.result = "Down " + sprite.key;
+
+
+  //     console.log('down', sprite.key);
+
+  // }
+
+  //  onDragStart(sprite, pointer) {
+
+  //     this.result = "Dragging " + sprite.key;
+
+  // }
+
+  //  onDragStop(sprite, pointer) {
+
+  //     this.result = sprite.key + " dropped at x:" + pointer.x + " y: " + pointer.y;
+
+  //     if (pointer.y > 400)
+  //     {
+  //         console.log('input disabled on', sprite.key);
+  //         sprite.input.enabled = false;
+
+  //         sprite.sendToBack();
+  //     }
+
+  // }
+
+  //  render() {
+
+  //     this.debug.text(this.result, 10, 20);
+
+  // }
+
+
+
 }
 
 @Component({
@@ -173,7 +218,7 @@ export class GameComponent implements OnInit {
   constructor() {
     this.config = {
       type: Phaser.AUTO,
-      scene: [ NewScene ],
+      scene: [NewScene],
       backgroundColor: '#4488aa',
       scale: {
         mode: Phaser.Scale.FIT,
