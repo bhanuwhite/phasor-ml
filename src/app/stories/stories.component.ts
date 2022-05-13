353360808069d7ca6bab7db4c0f777d2252852d4
shivam <shivam.ahex@gmail.com>
2022-05-13 15:37:31 +0530

    word appear one by one in madlibs story

--- src/app/stories/stories.component.ts
+++ src/app/stories/stories.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
@@ -9,17 +9,33 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class StoriesComponent implements OnInit {
 
+  // @ViewChild('story1') story1: ElementRef=;
+  story1: string;
+  someHtmlCode: any = '';
   matchObj: object = {}
+  i: number = 0;
+  speed: number = 100;
 
   constructor(private cdr: ChangeDetectorRef, private modalService: NgbModal, private router: Router) {
     let Match = JSON.parse(localStorage.getItem('Match'));
     Match.map((data) => {
       this.matchObj[Object.keys(data)[0]] = data[Object.keys(data)[0]]
     })
+    this.story1 = `Last week, our teacher told the class about an exciting new project. We get to build our own community garden! Mari asked around to see what her friends planned to bring. She wrote down notes on a <b>${this.matchObj['Noun1']}</b>. Sofia wants to plant <b>${this.matchObj['Prural Noun']}</b>. John is going to bring a <b>${this.matchObj['Noun2']}</b> from his pet <b>${this.matchObj['Animal']}</b>. Finally, Mari decided she will plant <b>${this.matchObj['Adjective']}</b> flowers.`
   }
 
   ngOnInit(): void {
+    const myInterval = setInterval(() => { this.getStory() }, this.speed);
+  }
 
+  getStory() {
+    try {
+      if (this.i < this.story1.length) {
+        this.someHtmlCode += this.story1.charAt(this.i);
+        this.i++;
+        this.cdr.detectChanges();
+      }
+    } catch (e) { }
   }
 
   open(content) {
