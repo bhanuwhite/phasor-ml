814432b94dabe0233aff64b4edb0cc84da8cc69c
shivam <shivam.ahex@gmail.com>
2022-05-17 18:48:50 +0530

    add button to train game

--- src/app/train-scroller-game/train-scroller-game.component.ts
+++ src/app/train-scroller-game/train-scroller-game.component.ts
@@ -14,6 +14,11 @@ class NewScene extends Phaser.Scene {
   tree1: any;
   tree2: any;
   tree3: any;
+  play: any;
+  pause: any;
+  rewind: any;
+  forward: any;
+  playStatus: boolean = false;
 
   preload() {
     this.load.image("background", "assets/images/train/bg.png");
@@ -29,10 +34,17 @@ class NewScene extends Phaser.Scene {
     this.load.image("car4", "assets/images/train/car-yellow.png");
     this.load.image("engine", "assets/images/train/engine.png");
     this.load.image('tracks', 'assets/images/train/tracks.png');
+    this.load.image('play', 'assets/images/train/play.png');
+    this.load.image('pause', 'assets/images/train/pause.png');
+    this.load.image('rewind', 'assets/images/train/rewind.png');
+    this.load.image('forward', 'assets/images/train/forward.png');
   }
 
   create() {
     this.background = this.add.tileSprite(0, 0, 0, 0, "background").setOrigin(0, 0.1);
+    this.background.displayWidth = this.sys.canvas.width;
+    this.background.displayHeight = this.sys.canvas.height;
+
     this.cloud1 = this.add.tileSprite(0, 0, 1000, 100, "cloud1").setOrigin(0, 0);
     this.cloud2 = this.add.tileSprite(0, 120, 1000, 90, "cloud2").setOrigin(0, 0);
 
@@ -49,55 +61,103 @@ class NewScene extends Phaser.Scene {
     this.add.sprite(604, 380, 'car4').setDisplaySize(70, 50);
     this.add.sprite(670, 380, 'engine').setDisplaySize(70, 50);
 
-    this.background.displayWidth = this.sys.canvas.width;
-    this.background.displayHeight = this.sys.canvas.height;
-
-    // this.text = this.add.text(1000, 50, "I got up in the middle of the night to get night to get a glass of water. I was really thirsty. I thought I saw a monster in the corner  of my room but it was just my cat up on the shelf.", { font: "16px Arial Black", fill: "#FF0000" });
-
-    // -----------Add Text-----------
+    // -----------Add Text--------------
     var style = {
-      'font': '36px Arial',
+      'word-spacing': '10px',
+      'font': '45px Arial',
       'font-weight': 'bold',
       'top': "50px",
-      'width': '300%',
+      'width': '505%',
       'position': 'absolute',
-      'left': '260%'
+      'left': '355%'
     };
-
-    let replaceWord = 'crayons'
 
     var div = document.createElement('div');
     div.innerHTML = `I got up in the middle of the night to get a glass of crayons. I was really thirsty. I thought I saw a monster in the corner  of my room but it was just my cat up on the shelf.`
 
     Object.assign(div.style, style);
     this.text = this.add.dom(0, 0, div);
+    // ------------------------------------------
 
-    this.input.keyboard.on('keydown-SPACE', function (event) {
-      this.backgroundImg = 'play'
-    }, this);
+    // -----------set Text Positiob------------
+    let textCoordinates = localStorage.getItem('textCoordinates')
+    this.text.x = textCoordinates
 
-    this.input.keyboard.on('keydown-R', function (event) {
-      this.backgroundImg == 'reverse' ? this.backgroundImg = 'play' : this.backgroundImg = 'reverse';
-
-      this.backgroundImg == 'reverse' ?
-        div.innerHTML = `I got up in the middle of the night to get a glass of <span style="color:red">crayons</span>. I was really thirsty. I thought I saw a monster in the corner  of my room but it was just my cat up on the shelf.` :
-        div.innerHTML = `I got up in the middle of the night to get a glass of <span style="color:green">water</span>. I was really thirsty. I thought I saw a monster in the corner  of my room but it was just my cat up on the shelf.`
-    }, this);
-
-    this.input.keyboard.on('keydown-P', function (event) {
-
-      if (this.backgroundImg == 'pause') {
-        this.backgroundImg = 'play'
-      } else {
+    if (this.playStatus == true) {
+      this.play = this.add.sprite(500, 465, 'play').setInteractive({ cursor: 'pointer' }).setDisplaySize(70, 70);
+      this.play.on('pointerup', () => {
         this.backgroundImg = 'pause'
-      }
+        this.playStatus = false;
+        this.scene.restart();
+      }, this)
+    }
 
-    }, this);
+    if (this.playStatus == false) {
+      div.innerHTML = `I got up in the middle of the night to get a glass of <span style="color:red">crayons</span>. I was really thirsty. I thought I saw a monster in the corner  of my room but it was just my cat up on the shelf.`
+      this.pause = this.add.sprite(500, 465, 'pause').setInteractive({ cursor: 'pointer' }).setDisplaySize(70, 70);
+      this.pause.on('pointerup', () => {
+        this.backgroundImg = 'play'
+        this.playStatus = true;
+        this.scene.restart();
+      }, this)
+    }
+
+    this.rewind = this.add.sprite(430, 460, 'rewind').setInteractive({ cursor: 'pointer' }).setDisplaySize(70, 50);
+    this.rewind.on('pointerup', () => {
+      this.backgroundImg = 'reverse';
+      div.innerHTML = `I got up in the middle of the night to get a glass of <span style="color:red">crayons</span>. I was really thirsty. I thought I saw a monster in the corner  of my room but it was just my cat up on the shelf.`
+    }, this)
+
+    this.forward = this.add.sprite(570, 460, 'forward').setInteractive({ cursor: 'pointer' }).setDisplaySize(70, 50);
+    this.forward.on('pointerup', () => {
+      this.backgroundImg = 'play';
+      div.innerHTML = `I got up in the middle of the night to get a glass of <span style="color:green">water</span>. I was really thirsty. I thought I saw a monster in the corner  of my room but it was just my cat up on the shelf.`
+    }, this)
+
+    // this.text = this.add.text(1000, 50, "I got up in the middle of the night to get night to get a glass of water. I was really thirsty. I thought I saw a monster in the corner  of my room but it was just my cat up on the shelf.", { font: "16px Arial Black", fill: "#FF0000" });
+
+    // // -----------Add Text-----------
+    // var style = {
+    //   'font': '36px Arial',
+    //   'font-weight': 'bold',
+    //   'top': "50px",
+    //   'width': '300%',
+    //   'position': 'absolute',
+    //   'left': '260%'
+    // };
+
+    // var div = document.createElement('div');
+    // div.innerHTML = `I got up in the middle of the night to get a glass of crayons. I was really thirsty. I thought I saw a monster in the corner  of my room but it was just my cat up on the shelf.`
+
+    // Object.assign(div.style, style);
+    // this.text = this.add.dom(0, 0, div);
+
+    // this.input.keyboard.on('keydown-SPACE', function (event) {
+    //   this.backgroundImg = 'play'
+    // }, this);
+
+    // this.input.keyboard.on('keydown-R', function (event) {
+    //   this.backgroundImg == 'reverse' ? this.backgroundImg = 'play' : this.backgroundImg = 'reverse';
+
+    //   this.backgroundImg == 'reverse' ?
+    //     div.innerHTML = `I got up in the middle of the night to get a glass of <span style="color:red">crayons</span>. I was really thirsty. I thought I saw a monster in the corner  of my room but it was just my cat up on the shelf.` :
+    //     div.innerHTML = `I got up in the middle of the night to get a glass of <span style="color:green">water</span>. I was really thirsty. I thought I saw a monster in the corner  of my room but it was just my cat up on the shelf.`
+    // }, this);
+
+    // this.input.keyboard.on('keydown-P', function (event) {
+
+    //   if (this.backgroundImg == 'pause') {
+    //     this.backgroundImg = 'play'
+    //   } else {
+    //     this.backgroundImg = 'pause'
+    //   }
+
+    // }, this);
+
   }
 
   update() {
-    if (this.text.x >= -4000) {
-      console.log("1", this.text.x)
+    if (this.text.x >= -5000) {
       this.backgroundImg == 'play' ? this.text.x -= 0.5 : this.backgroundImg == 'pause' ? this.text.x -= 0 : '';
     } else {
       this.text.x = 0
@@ -106,11 +166,10 @@ class NewScene extends Phaser.Scene {
     if (this.backgroundImg == 'reverse') {
       this.text.x += 0.5;
       if (this.text.x >= 0) {
-        console.log("2", this.text.x)
-        this.text.x = -4000;
+        this.text.x = -5000;
       }
     }
-
+    localStorage.setItem("textCoordinates", this.text.x)
     // -----------tree1------------
 
     if (this.tree1.x > -200) {
@@ -222,6 +281,7 @@ export class TrainScrollerGameComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    localStorage.clear()
     this.phaserGame = new Phaser.Game(this.config);
   }
 
