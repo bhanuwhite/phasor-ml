591b75a1dcfacc4ad0575656fa5fe00923d42c54
anurag <anuragpurwar007@gmail.com>
2022-05-17 15:03:48 +0530

    chnage bg on sticker book

--- src/app/stickerbook/stickerbook.component.ts
+++ src/app/stickerbook/stickerbook.component.ts
@@ -31,6 +31,27 @@ class NewScene extends Phaser.Scene {
     if (this.parentObject.backgroundName === "bg1") {
       this.load.image("background", "assets/images/train/bg.png");
     }
+    if (this.parentObject.backgroundName === "bg2") {
+      this.load.image("background", "assets/images/background/bg2.png");
+    }
+    if (this.parentObject.backgroundName === "bg3") {
+      this.load.image("background", "assets/images/background/bg3.png");
+    }
+    if (this.parentObject.backgroundName === "bg4") {
+      this.load.image("background", "assets/images/background/bg4.png");
+    }
+    if (this.parentObject.backgroundName === "bg5") {
+      this.load.image("background", "assets/images/background/bg5.png");
+    }
+    if (this.parentObject.backgroundName === "bg6") {
+      this.load.image("background", "assets/images/background/bg6.png");
+    }
+    if (this.parentObject.backgroundName === "bg7") {
+      this.load.image("background", "assets/images/background/bg7.png");
+    }
+    if (this.parentObject.backgroundName === "bg8") {
+      this.load.image("background", "assets/images/background/bg8.png");
+    }
 
     // this.load.image('p1', "assets/images/horseout.png");
     // this.load.image('p6', "ass ets/images/eleout.png");
@@ -46,13 +67,16 @@ class NewScene extends Phaser.Scene {
   }
 
   create() {
-
+    if (this.parentObject.backgroundName !== "blank") {
+      this.load.image("background", "assets/images/background/background.png");
+   
     this.background = this.add.image(0, 0, "background")
       .setOrigin(0, 0);
 
     // // // Based on your game size, it may "stretch" and distort.
     this.background.displayWidth = this.sys.canvas.width;
     this.background.displayHeight = this.sys.canvas.height;
+  }
 
     let image2 = this.add.sprite(600, 400, 'p2').setInteractive({ cursor: 'pointer' }).setDisplaySize(280, 150);
 
@@ -61,6 +85,8 @@ class NewScene extends Phaser.Scene {
     let image4 = this.add.sprite(1300, 400, 'p4').setInteractive({ cursor: 'pointer' }).setDisplaySize(280, 150);
     let image5 = this.add.sprite(1600, 400, 'p5').setInteractive({ cursor: 'pointer' }).setDisplaySize(280, 150);
     // let image4 = this.add.sprite(1400, 400, 'p4').setInteractive({ cursor: 'pointer' }).setDisplaySize(280, 150);
+
+    // image2.setDepth(1)
 
     this.input.setDraggable(image2);
     this.input.setDraggable(image3);
@@ -84,7 +110,7 @@ class NewScene extends Phaser.Scene {
     });
 
     this.input.on('dragend', function (pointer, gameObject) {
-
+      // image2.visible=false
       // if(gameObject.texture.key=='p2'){
       //   gameObject.x = 200;
       //   gameObject.y = 150 ;
@@ -119,7 +145,7 @@ export class StickerbookComponent implements OnInit {
   config: Phaser.Types.Core.GameConfig;
   color = 'red';
   showPanal = true;
-  backgroundName: any = 'bg'
+  backgroundName: any = 'blank'
   @ViewChild('content') content;
 
   constructor(private router: Router, private modalService: NgbModal) {
